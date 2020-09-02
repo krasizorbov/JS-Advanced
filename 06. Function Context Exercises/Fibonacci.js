cc9c99417fc4fb43e593d7e55c8489f482c8af35
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-02 13:03:01 +0300

    Added Fibonacci

---
+++ 06. Function Context Exercises/Fibonacci.js
@@ -0,0 +1,20 @@
+function getFibonator() {
+  let current = 0;
+  let next = 1;
+
+  return () => {
+    let nextNumber = current + next;
+    current = next;
+    next = nextNumber;
+
+    return current;
+  };
+}
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
