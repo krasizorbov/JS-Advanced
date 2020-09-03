89e294c95f51075fcbcf793217d69fa23ee1104f
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-03 08:17:25 +0300

    Added Table

--- 06. Function Context Exercises/06. JS-Advanced-Exercise-Sceleton/index.js
+++ 06. Function Context Exercises/06. JS-Advanced-Exercise-Sceleton/index.js
@@ -1,3 +1,18 @@
-function solve(){
-   // TODO
+function solve() {
+  let tr = document.querySelectorAll("tr");
+  let tbody = document.querySelector("tbody");
+  tbody.addEventListener("click", highLightRow);
+
+  function highLightRow(e) {
+    const change = e.target.parentNode;
+    console.log(change);
+    if (change.nodeName === "TR") {
+      if (change.style.backgroundColor !== "") {
+        change.style.backgroundColor = "";
+      } else {
+        [...tr].forEach((tr) => (tr.style.backgroundColor = ""));
+        change.style.backgroundColor = "#413f5e";
+      }
+    }
+  }
 }
