0da9a1d4b876e54dd81fb533855701769043396f
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-04 17:05:28 +0300

    Added Argument Info

---
+++ 07. Advanced Functions Exercises/Argument info.js
@@ -0,0 +1,22 @@
+function solution() {
+  let result = [];
+  let count = {};
+  for (let i = 0; i < arguments.length; i++) {
+    let type = typeof arguments[i];
+    let value = arguments[i];
+    result.push({ type: type, value: value });
+    if (!count.hasOwnProperty(type)) {
+      count[type] = 1;
+    } else {
+      count[type]++;
+    }
+  }
+  result.forEach((obj) => console.log(`${obj.type}: ${obj.value}`));
+  let sorted = Object.entries(count).sort((a, b) => b[1] - a[1]);
+  for (const key in sorted) {
+    console.log(`${sorted[key][0]} = ${sorted[key][1]}`);
+  }
+}
+solution("cat", 42, function () {
+  console.log("Hello world!");
+});
