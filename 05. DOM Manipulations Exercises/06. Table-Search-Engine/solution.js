7f2fc9f1e7f0907363ce5a6a7523f60d306d1407
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-05 07:29:02 +0300

    Added Table Search Engine

--- 05. DOM Manipulations Exercises/06. Table-Search-Engine/solution.js
+++ 05. DOM Manipulations Exercises/06. Table-Search-Engine/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-   //TODO...
+  let tr = document.querySelectorAll("tbody tr");
+  let textInput = document.querySelector("#searchField");
+  let searchBtn = document.querySelector("#searchBtn");
+  searchBtn.addEventListener("click", function (e) {
+    let text = textInput.value;
+    for (const r of tr) {
+      if (r.textContent.includes(text)) {
+        r.classList.add("select");
+      } else {
+        r.classList = "";
+      }
+    }
+    textInput.value = "";
+  });
 }
