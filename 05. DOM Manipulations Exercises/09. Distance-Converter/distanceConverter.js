78e787e9e6ae5a071e6110effdf6667cb7742589
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-07 12:36:21 +0300

    Added Distance Converter

--- 05. DOM Manipulations Exercises/09. Distance-Converter/distanceConverter.js
+++ 05. DOM Manipulations Exercises/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,24 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+  let converter = {
+    km: 1000,
+    m: 1,
+    cm: 0.01,
+    mm: 0.001,
+    mi: 1609.34,
+    yrd: 0.9144,
+    ft: 0.3048,
+    in: 0.0254,
+  };
+  let inputNumber = document.getElementById("inputDistance");
+  let outputNumber = document.getElementById("outputDistance");
+  let convertBtn = document.getElementById("convert");
+  let inputOption = document.getElementById("inputUnits");
+  let outputOption = document.getElementById("outputUnits");
+
+  convertBtn.addEventListener("click", function (e) {
+    let fromUnit = inputOption.value;
+    let toUnit = outputOption.value;
+    let input = Number(inputNumber.value);
+    outputNumber.value = (input * converter[fromUnit]) / converter[toUnit];
+  });
 }
