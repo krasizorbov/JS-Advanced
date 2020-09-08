ed2e270d4da5416218f4dc18ab602b23b7bfd8d8
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-09 02:48:06 +0300

    Refactoring

--- 04. DOM Lab/04. Visited Websites/solution.js
+++ 04. DOM Lab/04. Visited Websites/solution.js
@@ -1,12 +1,11 @@
 function solve() {
-  const clickElement = (i) => {
-    const paragraphs = document.getElementsByTagName("p");
-    const p = paragraphs[i];
-    const increaseNum = Number(p.textContent.match(/\d+/g)) + 1;
-    p.innerHTML = `visited ${increaseNum} times`;
-  };
-  const a = document.getElementsByTagName("a");
+  let a = document.querySelectorAll("a");
   for (let i = 0; i < a.length; i++) {
-    a[i].addEventListener("click", () => clickElement(i));
+    a[i].addEventListener("click", function (e) {
+      let p = document.querySelectorAll("p");
+      p = p[i];
+      const increaseNum = Number(p.innerHTML.match(/\d+/g)) + 1;
+      p.innerHTML = `visited ${increaseNum} times`;
+    });
   }
 }
