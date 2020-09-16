0919b934cf7bf2e51974390619608651be9d4cfc
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-16 10:03:53 +0300

    Added Dropdown Menu

--- 06. Function Context Lab/06. JS-Advanced-Lab-Sceleton/app.js
+++ 06. Function Context Lab/06. JS-Advanced-Lab-Sceleton/app.js
@@ -1,3 +1,27 @@
 function solve() {
-    // ToDo
+  const box = document.querySelector("#box");
+  let dropDown = document.querySelector("#dropdown");
+  let dropDownList = document.querySelector("#dropdown-ul");
+
+  dropDown.addEventListener("click", () => {
+    if (
+      dropDownList.style.display === "" ||
+      dropDownList.style.display === "none"
+    ) {
+      dropDownList.style.display = "block";
+    } else {
+      dropDownList.style.display = "none";
+    }
+    if (dropDownList.style.display === "none") {
+      box.style.backgroundColor = "black";
+      box.style.color = "white";
+    }
+  });
+
+  Array.from(dropDownList.querySelectorAll("li")).forEach((el) =>
+    el.addEventListener("click", function (e) {
+      box.style.backgroundColor = e.target.textContent;
+      box.style.color = "black";
+    })
+  );
 }
