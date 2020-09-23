f07c8a7ed31bf20b2c18681d9af433c7d610d52f
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-09-23 11:02:20 +0300

    Refactoring

--- 02. Arrays Exercises/Magic Matrices.js
+++ 02. Arrays Exercises/Magic Matrices.js
@@ -1,21 +1,48 @@
 function solution(input = []) {
   let sum = input[0].reduce((a, c) => a + c, 0);
   const arrayColumn = (arr, n) => arr.map((x) => x[n]);
-  let isValid = true;
   for (let i = 0; i < input.length; i++) {
     let column = arrayColumn(input, i);
     let verticalSum = column.reduce((a, c) => a + c, 0);
     let horizontalSum = input[i].reduce((a, c) => a + c, 0);
     if (sum !== verticalSum || sum !== horizontalSum) {
-      isValid = false;
       console.log("false");
       return;
     }
   }
   console.log("true");
 }
+// Решение на Vkirivpz
+// function solution(input) {
+//   let arr = [];
+//   let rowSum = 0;
+//   let colSum = 0;
+//   let isTrue;
+//   let sum = input[0].reduce((a, c) => a + c, 0);
+//   // filling the matrix
+//   for (let row = 0; row < input.length; row++) {
+//     arr.push(input[row]);
+//   }
+
+//   for (let row = 0; row < arr.length; row++) {
+//     rowSum = arr[row].reduce((curr, next) => curr + next, 0);
+
+//     for (let col = 0; col < arr.length; col++) {
+//       colSum += arr[col][row];
+//     }
+
+//     if (sum !== rowSum || sum !== colSum) {
+//       isTrue = false;
+//       console.log("false");
+//       return;
+//     } else {
+//       colSum = 0;
+//     }
+//   }
+//   console.log("true");
+// }
 solution([
-  [11, 32, 45],
-  [21, 0, 1],
-  [21, 1, 1],
+  [1, 0, 0],
+  [0, 0, 1],
+  [0, 1, 0],
 ]);
