9745e51950193bdee726f9d7f42b92df80add8d3
Krasi Zorbov <krasi.zorbov@abv.bg>
2020-10-19 14:20:51 +0300

    Refactoring

--- 10. Side Scrolling Game/main.js
+++ 10. Side Scrolling Game/main.js
@@ -11,8 +11,8 @@ document.addEventListener("keyup", onKeyUp);
 
 let keys = {};
 let player = {x: 150, y: 100, width: 0, heigth: 0, lastTimeFiredFireball: 0};
-let game = {speed: 2, movingMultiplier: 4, fireBallMultiplier: 5, fireInterval: 1000, cloudSpawnInterval: 3000};
-let scene = {score: 0, lastCloudSpawn: 0};
+let game = {speed: 2, movingMultiplier: 4, fireBallMultiplier: 5, fireInterval: 1000, cloudSpawnInterval: 3000, bugSpawnInterval: 1000, bugKillBonus: 2000};
+let scene = {score: 0, lastCloudSpawn: 0, lastBugSpawn: 0, isActiveGame: true};
 
 function onKeyDown(e){
     keys[e.code] = true;
@@ -42,6 +42,20 @@ function gameAction(timestamp){
     const wizard = document.querySelector(".wizard");
     scene.score++;
 
+    
+
+    if (timestamp - scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random()) {
+        let bug = document.createElement("div");
+    bug.classList.add("bug");
+    bug.x = gameArea.offsetWidth - 60;
+    bug.style.left = bug.x + "px";
+    bug.style.top = (gameArea.offsetHeight - 60) * Math.random() + "px";
+    gameArea.appendChild(bug);
+    scene.lastBugSpawn = timestamp;
+    }
+
+
+
     if (timestamp - scene.lastCloudSpawn > game.cloudSpawnInterval + 20000 * Math.random()) {
         let cloud = document.createElement("div");
     cloud.classList.add("cloud");
@@ -51,6 +65,7 @@ function gameAction(timestamp){
     gameArea.appendChild(cloud);
     scene.lastCloudSpawn = timestamp;
     }
+
     let clouds = document.querySelectorAll(".cloud");
     clouds.forEach(c => {
         c.x -= game.speed;
@@ -68,6 +83,26 @@ function gameAction(timestamp){
     if (fb.x + fb.offsetWidth > gameArea.offsetWidth) {
         fb.parentElement.removeChild(fb);
     }});
+
+    let bugs = document.querySelectorAll(".bug");
+bugs.forEach(b => {
+    b.x -= game.speed * 3;
+    b.style.left = b.x + "px";
+    if (b.x + bugs.offsetWidth <= 0) {
+        b.parentElement.removeChild(b);
+    }
+    if (collisionDetection(wizard, b)) {
+        gameOverAction();
+    }
+    fireBalls.forEach(fb =>{
+        if (collisionDetection(fb, b)) {
+            scene.score += game.bugKillBonus;
+            b.parentElement.removeChild(b);
+            fb.parentElement.removeChild(fb);
+        }
+    })
+    
+})
 
     let isInAir = (player.y + player.height) <= gameArea.offsetHeight;
     if (isInAir) {
@@ -95,7 +130,10 @@ function gameAction(timestamp){
     wizard.style.top = player.y + 'px';
     wizard.style.left = player.x + "px";
     gamePoints.textContent = scene.score;
-    window.requestAnimationFrame(gameAction);
+    //window.requestAnimationFrame(gameAction);
+    if (scene.isActiveGame) {
+        window.requestAnimationFrame(gameAction);
+    }
 }
 function addFireBall(){
     let fireBall = document.createElement("div");
@@ -104,4 +142,15 @@ function addFireBall(){
     fireBall.x = player.x + player.width;
     fireBall.style.left = fireBall.x + "px";
     gameArea.appendChild(fireBall);
+}
+
+function collisionDetection(firstElement, secondElement){
+    let firstRect = firstElement.getBoundingClientRect();
+    let secondRect = secondElement.getBoundingClientRect();
+    return !(firstRect.top > secondRect.bottom || firstRect.bottom < secondRect.top || firstRect.right < secondRect.left || firstRect.left > secondRect.right);
+}
+
+function gameOverAction(){
+    scene.isActiveGame = false;
+    gameOver.classList.remove("hide");
 }
